d118b08777521a12cf9b0230a65320bf20d6ce79
mahmoudabdelrady248 <58746233+mahmoudabdelrady248@users.noreply.github.com>
2021-12-22 00:29:19 +0200

    Add files via upload

---
+++ my_secret_keeper/src/components/CreateDiary.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>Hello</div>
+  <form @submit.prevent="pressed">
+    <div class="body">
+      <input type="text" v-model="body" placeholder="body">
+    </div>
+    <div class="diary_title">
+      <input type="text" v-model="diary_title" placeholder="diary_title">
+    </div>
+    <button type="submit">CreateDiary</button>
+  </form>
+</template>
+<script>
+import DiaryRepository from "../data/user/repository/diary_repository.vue"
+export default {
+  name: 'CreateDiary',
+  methods:{
+    async pressed(){
+      await DiaryRepository.methods.createDiary(this.body,this.diary_title);
+    }
+  },
+  data(){
+    return{
+      body:"",
+      diary_title:""
+    }
+  }
+}
+</script>
