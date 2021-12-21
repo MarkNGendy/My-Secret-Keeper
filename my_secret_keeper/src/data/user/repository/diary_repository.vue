d9d49dcb744196839961969ccb132d346588237b
mahmoudabdelrady248 <58746233+mahmoudabdelrady248@users.noreply.github.com>
2021-12-21 16:55:09 +0200

    Add files via upload

---
+++ my_secret_keeper/src/data/user/repository/diary_repository.vue
@@ -0,0 +1,14 @@
+<script>
+import diary_firebase_manager from '../firebase/diary_firebase_manager.vue'
+export default {
+    name: "diary-repository",
+    methods: {
+        async createDiary() {
+            await diary_firebase_manager.methods.createDiary();
+        },
+        async updateDiary(){
+            await diary_firebase_manager.methods.updateDiary();
+        }
+    }
+}
+</script>
