d13b2349d6cf956269a5e85f691ba6a8f3c7a59e
Mark Nader <mark.n.gendy@gmail.com>
2021-12-22 20:07:08 +0200

    Creating new project with vuetify

--- my_secret_keeper/src/components/HelloWorld.vue
+++ my_secret_keeper/src/components/HelloWorld.vue
@@ -27,9 +27,8 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-  </div>
-  <div>
   <button v-on:click= "testSignIn">Add 1</button>
+
   </div>
 </template>
 
--- my_secret_keeper/src/main.js
+++ my_secret_keeper/src/main.js
@@ -1,26 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+Vue.config.productionTip = false;
 
-import router from './router'
-//import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCcEVAnqFVhfaDIaJL7tJACzZ8sXOJUGTM",
-  authDomain: "my-secret-keeper-dev.firebaseapp.com",
-  projectId: "my-secret-keeper-dev",
-  storageBucket: "my-secret-keeper-dev.appspot.com",
-  messagingSenderId: "129370946291",
-  appId: "1:129370946291:web:91c86e38fbafe7999f5586",
-  measurementId: "G-1W46232DB7"
-};
-
-initializeApp(firebaseConfig);
-
-createApp(App).use(router).mount('#app')
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
