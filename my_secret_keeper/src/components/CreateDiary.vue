53aa8a20fbf5ff73a7855b0aff63f30b07907ed2
mark74384 <58529681+mark74384@users.noreply.github.com>
2021-12-23 21:27:41 +0200

    create diary demo

--- my_secret_keeper/src/components/CreateDiary.vue
+++ my_secret_keeper/src/components/CreateDiary.vue
@@ -1,51 +1,94 @@
 <template>
   <div>
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
+    <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on, attrs }">
         <v-btn
-            class="mx-10 ml-10 mt-4"
-            small
-            dark
-            fab
-            color="cyan"
-            v-bind="attrs"
-            v-on="on"
+          class="mx-10 ml-10 mt-4"
+          small
+          dark
+          fab
+          color="cyan"
+          v-bind="attrs"
+          v-on="on"
         >
-         
-      <v-icon class="">
-        mdi-pencil
-      </v-icon>
-    </v-btn>
+          <v-icon class=""> mdi-pencil </v-icon>
+        </v-btn>
       </template>
 
       <v-card>
         <v-card-title class="text-h5 grey lighten-2">
           create new diary
         </v-card-title>
-        <div>Hello</div>
-            <form @submit.prevent="pressed">
-                <div class="body">
-                <input type="text" v-model="body" placeholder="body">
-                </div>
-                <div class="diary_title">
-                <input type="text" v-model="diary_title" placeholder="diary_title">
-                </div>
-                <button type="submit">CreateDiary</button>
-            </form>
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field
+              v-model="title"
+              label="Title"
+              prepend-icon="mdi-folder"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="catagory"
+              label="catagory"
+              prepend-icon="mdi-folder"
+            ></v-text-field>
+
+
+            <v-textarea
+              v-model="content"
+              label="Diary"
+              prepend-icon="mdi-PencilPlus "
+            ></v-textarea>
+
+            
+            
+            <v-row justify="center">
+              <v-date-picker
+                v-model="date"
+                :allowed-dates="allowedDates"
+                class="mt-4"
+                min="2016-06-15"
+                max="2030-03-20"
+                @change="menu=false"
+              ></v-date-picker>
+            </v-row>
+
+            <v-spacer></v-spacer>
+
+            <v-btn flat @click="submit" class="success mx-0 mt-3"
+              >Add Diary</v-btn
+            >
+          </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        dialog: false,
-      }
+import format from 'date-fns/format'
+export default {
+  data() {
+    return {
+      title: "",
+      content: "",
+      date: null,
+      menu: false,
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.title,this.catagory, this.content, this.date);
     },
+    methods: {
+      allowedDates: (val) => parseInt(val.split("-")[2], 10) % 2 === 0,
+    },
+  },
+  computed: {
+    formattedDate () {
+      console.log(this.date)
+      return this.due ? format(this.due, 'Do MMM YYYY') : ''
+    }
   }
+};
 </script>
