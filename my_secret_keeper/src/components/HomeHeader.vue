d6a3cdccca50f048cdf91e116fc088c6aa88a3a6
mina-henen <mon.messi6@gmail.com>
2021-12-23 19:02:05 +0200

    avatar

--- my_secret_keeper/src/components/HomeHeader.vue
+++ my_secret_keeper/src/components/HomeHeader.vue
@@ -8,17 +8,25 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="cyan lighten-4">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img class="text-lg-center" src="/avatar.png">
+          </v-avatar>
+          <p class="black--text subheading mt-1">Dummy user</p>
+        </v-flex>
+      </v-layout>
      <v-list>
-       <v-list-item-group>
-        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-item-icon>
-            <v-icon v-text="link.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="link.text"></v-list-item-title>
-          </v-list-item-content>
+        <v-list-item-group>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-icon>
+              <v-icon v-text="link.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="link.text"></v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
-          </v-list-item-group>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
     </nav>
