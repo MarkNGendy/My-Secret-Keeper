7b76c6b14a88c063aca6e7d2ebedb04dcd095c5e
mahmoudabdelrady248 <58746233+mahmoudabdelrady248@users.noreply.github.com>
2021-12-30 13:33:45 +0200

    Update diary_repository.vue

--- my_secret_keeper/src/data/user/repository/diary_repository.vue
+++ my_secret_keeper/src/data/user/repository/diary_repository.vue
@@ -1,13 +1,40 @@
 <script>
 import diary_firebase_manager from '../firebase/diary_firebase_manager.vue'
+
 export default {
     name: "diary-repository",
     methods: {
-        async createDiary() {
-            await diary_firebase_manager.methods.createDiary();
+        async createDiary(body,diary_title) {
+            if(body=="")
+                return "Enter Body"
+            if(diary_title=="")
+                return "Enter Diary Title"
+            await diary_firebase_manager.methods.createDiary(body,diary_title);
+            return "Correct Creation"
         },
-        async updateDiary(){
-            await diary_firebase_manager.methods.updateDiary();
+        async updateDiary(doc_id,body,diary_title){
+            if(body=="")
+                return "Enter Body"
+            if(diary_title=="")
+                return "Enter Diary Title"
+            try {
+               await diary_firebase_manager.methods.updateDiary(doc_id,body,diary_title);
+               return "Correct Update" 
+            } catch (error) {
+                //doc_id is not correct
+                return "Invalid Diary"
+            }
+            
+        },
+        async deleteDiary(doc_id){
+            try {
+                await diary_firebase_manager.methods.deleteDiary(doc_id);
+                return "Correct Delete"
+            } catch (error) {
+                //doc_id is not correct
+                return "Invalid Diary"
+            }
+            
         }
     }
 }
