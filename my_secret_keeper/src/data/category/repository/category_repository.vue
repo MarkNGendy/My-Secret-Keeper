a18f37c560fcff5f6425bbd8ae8ae11b99c7b9aa
mahmoudabdelrady248 <58746233+mahmoudabdelrady248@users.noreply.github.com>
2021-12-31 20:49:23 +0200

    Add files via upload

---
+++ my_secret_keeper/src/data/category/repository/category_repository.vue
@@ -0,0 +1,28 @@
+<script>
+import CategoryFirebaseManager from '../firebase/category_firebase_manager.vue'
+export default {
+    name: "category-repository",
+    methods: {
+        async createCategory(name,icon){
+            if(name === "") 
+                return "Please enter name for category";
+            if(icon === "") 
+                return "Please enter icon for category";
+            return await CategoryFirebaseManager.methods.createCategory(name,icon);
+        },
+        async updateCategory(id,name,icon){
+            if(name === "") 
+                return "Please enter name for category";
+            if(icon === "") 
+                return "Please enter icon for category";
+            return await CategoryFirebaseManager.methods.updateCategory(id,name,icon);
+        },
+        async deleteCategory(id){
+            return await CategoryFirebaseManager.methods.deleteCategory(id);
+        },
+        async retrieveDiaries() {
+            return await CategoryFirebaseManager.methods.retreiveCategories();
+        }
+    }
+}
+</script>
