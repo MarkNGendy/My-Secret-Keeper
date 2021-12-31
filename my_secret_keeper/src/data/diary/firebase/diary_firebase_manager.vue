5e5e707737a6664d7505c3134f8f46d9d4c88edd
mahmoudabdelrady248 <58746233+mahmoudabdelrady248@users.noreply.github.com>
2021-12-31 21:10:36 +0200

    Update diary_firebase_manager.vue

--- my_secret_keeper/src/data/diary/firebase/diary_firebase_manager.vue
+++ my_secret_keeper/src/data/diary/firebase/diary_firebase_manager.vue
@@ -5,28 +5,39 @@ export default {
     name: 'diary_firebase_manager',
     methods: {
         async createDiary(title, body, date){
-            await addDoc(collection(getFirestore(), "Diaries"), {
-                date: date,
-                body: body,
-                title:title,
-                user_id:getAuth().currentUser.uid,
-                category_id: "",
-                template_id: ""
-            });
-            
+            try {
+                await addDoc(collection(getFirestore(), "Diaries"), {
+                    date: date,
+                    body: body,
+                    title:title,
+                    user_id:getAuth().currentUser.uid,
+                    category_id:"",
+                    template_id:""
+                });
+                return "Diary Created successfully.";  
+            } catch (error) {
+                return "Invalid Operation"
+            }
         },
-        async updateDiary(doc_id,table_name){
-            console.log(doc_id);
-            await updateDoc(doc(getFirestore(),table_name,doc_id), {
-                body: "body updated",
-                diary_title:"diary updated"
-            });
-            this.retrieveDiaries();
+        async updateDiary(id,title, body, date){
+            try {
+                await updateDoc(doc(getFirestore(),"Diaries",id), {
+                    date: date,
+                    body: body,
+                    title:title
+                });
+                return "Diary Updated successfully."; 
+            } catch (error) {
+                return "Invalid Operation"
+            }
         },
-        async deleteDiary(doc_id,table_name){
-            console.log(doc_id);
-            await deleteDoc(doc(getFirestore(),table_name,doc_id));
-            this.retrieveDiaries();
+        async deleteDiary(doc_id){
+            try {
+                await deleteDoc(doc(getFirestore(),"Diaries",doc_id));
+                return "Diary Deleted successfully."; 
+            } catch (error) {
+                return "Invalid Operation";
+            }
         },
         async retrieveDiaries(){
             const querySnapshot = await getDocs(query(collection(getFirestore(), "Diaries"), where("user_id", "==",getAuth().currentUser.uid)));
