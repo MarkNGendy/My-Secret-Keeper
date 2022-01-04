44ea700f8cfac8adb393c152f50b1dbb00cfeceb
mina-henen <mon.messi6@gmail.com>
2022-01-04 16:12:09 +0200

    fixing navigation bug

--- my_secret_keeper/src/components/HomeHeader.vue
+++ my_secret_keeper/src/components/HomeHeader.vue
@@ -1,36 +1,21 @@
 <template>
 <nav>
     <v-toolbar app color="blue" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer" class="white--text"></v-app-bar-nav-icon>
       <v-toolbar-title style="font-weight:bold;font-size:25px">My Secret Keeper</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn rounded text @click= "$router.push('/home')">
+          Diaries
+      </v-btn>
+      <v-btn rounded text @click= "$router.push('/categories')" >
+          Categories
+      </v-btn>
       <CreateCategory></CreateCategory>
+      <v-btn rounded text @click= "$router.push('/templates')">
+          Templates
+      </v-btn>
       <CreateTemplate></CreateTemplate>
       <v-btn @click="logout" rounded text>Logout<v-icon right>mdi-arrow-right-bold-box-outline</v-icon></v-btn>
     </v-toolbar>
-
-    <v-navigation-drawer app v-model="drawer" class="cyan lighten-4">
-      <v-layout column align-center>
-        <v-flex class="mt-5">
-          <v-avatar size="100">
-            <img class="text-lg-center" src="/avatar.png">
-          </v-avatar>
-          <p class="black--text subheading mt-1">{{ userName }}</p>
-        </v-flex>
-      </v-layout>
-     <v-list>
-        <v-list-item-group>
-          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-            <v-list-item-icon>
-              <v-icon v-text="link.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="link.text"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
     </nav>
 </template>
 
